27fc341121efe650c7d3db641e4585515e869766
Marcelo Ramos <M.Ramos@SPOD2695.ymfsp.local>
2018-03-06 21:45:53 -0300

    segunda atualização

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/services/LivrosService.java
@@ -0,0 +1,31 @@
+package com.algaworks.socialbooks.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivrosRepository;
+import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
+
+@Service
+public class LivrosService {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	public List<Livro> listar() {
+		return livrosRepository.findAll();
+	}
+	
+	public Livro buscar(Long id) {
+		Livro livro = livrosRepository.findOne(id);
+		
+		if(livro == null) {
+			throw new LivroNaoEncontradoException("O livro não pode ser encontrado.");
+		}
+		return livro;
+	}
+
+}
